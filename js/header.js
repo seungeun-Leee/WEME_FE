c58f13308ee8b8bfe05637691ccb4d75c5017377
coolOlive <galilee155@naver.com>
2022-02-11 19:25:24 +0900

    header부분 색 변하고 subnav나옴

---
+++ js/header.js
@@ -0,0 +1,31 @@
+var headerView = document.querySelector(".header");
+
+headerView.addEventListener("mousemove",function() {
+    document.querySelector("#weme_subnav").style.display="block";
+    document.querySelector("#temp_subnav").style.display="block";
+    document.querySelector("#adop_subnav1").style.display="block";
+    document.querySelector("#adop_subnav2").style.display="block";
+    document.querySelector("#info_subnav").style.display="block";
+    document.querySelector(".header").style.paddingBottom="138px";
+    document.querySelector(".weme").style.height="200px";
+    document.querySelector(".temp").style.height="200px";
+    document.querySelector(".adop").style.height="200px";
+    document.querySelector(".info").style.height="200px";
+    document.querySelector(".user").style.height="200px";
+
+});
+
+headerView.addEventListener("mouseleave",function() {
+    document.querySelector("#weme_subnav").style.display="none";
+    document.querySelector("#temp_subnav").style.display="none";
+    document.querySelector("#adop_subnav1").style.display="none";
+    document.querySelector("#adop_subnav2").style.display="none";
+    document.querySelector("#info_subnav").style.display="none";
+    document.querySelector(".header").style.paddingBottom="0px";
+    document.querySelector(".weme").style.height="50px";
+    document.querySelector(".temp").style.height="50px";
+    document.querySelector(".adop").style.height="50px";
+    document.querySelector(".info").style.height="50px";
+    document.querySelector(".user").style.height="50px";
+});
+
